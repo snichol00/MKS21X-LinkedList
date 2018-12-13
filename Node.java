bbd0ca9d993e63dcc156f897d6455509b5fcd657
Sophie Nichol <snichol00@stuy.edu>
2018-12-12 21:10:28 -0500

    made Node constructor

--- Node.java
+++ Node.java
@@ -1,4 +1,10 @@
 public class Node{
   private int data;
   private Node next,prev;
+
+  public Node(int myData, Node nextNode, Node prevNode){
+    data = myData;
+    next = nextNode;
+    prev = prevNode;
+  }
 }
